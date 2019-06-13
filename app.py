eca44248ab0d932e2e10333b1c0107da60fe26d1
ayworld <ayo.tim.fola@gmail.com>
2019-06-12 22:52:07 -0500

    almost there

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
+
+topics = [
+    "Consumption",
+    "Production",
+    "Emissions"
+]
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    """Energy Evolution"""
+    return render_template("index.html")
+
+#api route for data to go to client
+@app.route("/ee/topics")
+
+# define function
+def maindata():
+    return jsonify(topics)
+
 if __name__ == "__main__":
     app.run()
+
