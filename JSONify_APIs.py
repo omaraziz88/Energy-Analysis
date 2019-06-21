6ba3d9f467df2fbf89eba6e9c99d4cc623834c79
ppmayer <ppmayer@gmail.com>
2019-06-20 19:46:26 -0500

    new geoJSON APIs

--- JSONify_APIs.py
+++ JSONify_APIs.py
@@ -5,20 +5,15 @@ import json
 
 engine = create_engine(os.environ.get("JAWSDB_URL"))
 
-with open('static/data/countries.geojson', 'r') as f:
-    countries_co2_emissions = json.load(f)
-    countries_NGL_prod = json.load(f)
-    countries_nat_gas_prod = json.load(f)
-    countries_solar_wind_share = json.load(f)
-    countries_total_elec_prod = json.load(f)
-
 latlon_df = pd.read_sql("SELECT * FROM LatLon_Table", engine)
 
 # CO2 emissions API
+with open('static/data/countries.geojson', 'r') as f:
+    countries_co2_emissions = json.load(f)
+
 CO2_emissions_df = pd.read_sql("SELECT * FROM CO2_Emissions", engine)
 CO2_emissions_df_merged = CO2_emissions_df.merge(latlon_df, how='inner', on='Country')
 CO2_emissions_json = json.loads(CO2_emissions_df_merged.to_json(orient="records"))
-
 # Loop over GeoJSON features and add the new properties
 for feat in countries_co2_emissions['features']:
     value_1990 = "NA"
@@ -43,11 +38,13 @@ for feat in countries_co2_emissions['features']:
 
 CO2_emissions_api = countries_co2_emissions
 
-# crue/NGL production API
+# crude/NGL production API
+with open('static/data/countries.geojson', 'r') as f:
+    countries_NGL_prod = json.load(f)
+
 Crude_NGL_Prod_df = pd.read_sql("SELECT * FROM Crude_NGL_Production", engine)
 Crude_NGL_Prod_df_merged=Crude_NGL_Prod_df.merge(latlon_df,how='inner',on='Country')
-Crude_NGL_Prod_json = json.load(Crude_NGL_Prod_df_merged.to_json(orient = "records"))
-
+Crude_NGL_Prod_json = json.loads(Crude_NGL_Prod_df_merged.to_json(orient = "records"))
 # Loop over GeoJSON features and add the new properties
 for feat in countries_NGL_prod['features']:
     value_1990 = "NA"
@@ -73,10 +70,12 @@ for feat in countries_NGL_prod['features']:
 Crude_NGL_Prod_api = countries_NGL_prod
 
 # Natural Gas Prod API
+with open('static/data/countries.geojson', 'r') as f:
+    countries_nat_gas_prod = json.load(f)
+
 Natural_Gas_Prod_df = pd.read_sql("SELECT * FROM Natural_Gas_Production", engine)
 Natural_Gas_Prod_df_merged=Natural_Gas_Prod_df.merge(latlon_df,how='inner',on='Country')
 Natural_Gas_Prod_json = json.loads(Natural_Gas_Prod_df_merged.to_json(orient = "records"))
-
 # Loop over GeoJSON features and add the new properties
 for feat in countries_nat_gas_prod['features']:
     value_1990 = "NA"
@@ -101,14 +100,95 @@ for feat in countries_nat_gas_prod['features']:
 
 Natural_Gas_Prod_api = countries_nat_gas_prod
 
-# Renewables_Electricity_Share_df = pd.read_sql("SELECT * FROM Renewables_Electricity_Share", engine)
-# Renewables_Electricity_Share_df_merged=Renewables_Electricity_Share_df.merge(latlon_df,how='inner',on='Country')
-# Renewables_Electricity_Share_json = json.loads(Renewables_Electricity_Share_df_merged.to_json(orient = "records"))
+# Renewable Share Production API
+with open('static/data/countries.geojson', 'r') as f:
+    countries_ren_elec_share = json.load(f)
 
-# Solar_and_Wind_Share_df = pd.read_sql("SELECT * FROM Solar_and_Wind_Share", engine)
-# Solar_and_Wind_Share_df_merged=Solar_and_Wind_Share_df.merge(latlon_df,how='inner',on='Country')
-# Solar_and_Wind_Share_json = json.loads(Solar_and_Wind_Share_df_merged.to_json(orient = "records"))
+Renewables_Electricity_Share_df = pd.read_sql("SELECT * FROM Renewables_Electricity_Share", engine)
+Renewables_Electricity_Share_df_merged=Renewables_Electricity_Share_df.merge(latlon_df,how='inner',on='Country')
+Renewables_Electricity_Share_json = json.loads(Renewables_Electricity_Share_df_merged.to_json(orient = "records"))
+# Loop over GeoJSON features and add the new properties
+for feat in countries_ren_elec_share['features']:
+    value_1990 = "NA"
+    value_2000 = "NA"
+    value_2010 = "NA"
+    value_2017 = "NA"
+    for i in range(len(Renewables_Electricity_Share_json)):
+        dict = Renewables_Electricity_Share_json[i]
+        if dict['Country'] == feat['properties']["ADMIN"]:
+            value_1990 = dict["1990"]
+            value_2000 = dict["2000"]
+            value_2010 = dict["2010"]
+            value_2017 = dict["2017"]
+    if value_1990:
+        feat['properties']["1990"] = value_1990
+    if value_2000:
+        feat['properties']["2000"] = value_2000
+    if value_2010:
+        feat['properties']["2010"] = value_2010
+    if value_2017:
+        feat['properties']["2017"] = value_2017
 
-# Total_Electricity_Prod_df = pd.read_sql("SELECT * FROM Total_Electricity_Production", engine)
-# Total_Electricity_Prod_df_merged=Total_Electricity_Prod_df.merge(latlon_df,how='inner',on='Country')
-# Total_Electricity_Prod_json = json.loads(Total_Electricity_Prod_df_merged.to_json(orient = "records"))
+Renewables_Electricity_Share_api = countries_ren_elec_share
+
+# solar wind share API
+with open('static/data/countries.geojson', 'r') as f:
+    countries_solar_wind_share = json.load(f)
+
+Solar_and_Wind_Share_df = pd.read_sql("SELECT * FROM Solar_and_Wind_Share", engine)
+Solar_and_Wind_Share_df_merged=Solar_and_Wind_Share_df.merge(latlon_df,how='inner',on='Country')
+Solar_and_Wind_Share_json = json.loads(Solar_and_Wind_Share_df_merged.to_json(orient = "records"))
+# Loop over GeoJSON features and add the new properties
+for feat in countries_solar_wind_share['features']:
+    value_1990 = "NA"
+    value_2000 = "NA"
+    value_2010 = "NA"
+    value_2017 = "NA"
+    for i in range(len(Solar_and_Wind_Share_json)):
+        dict = Solar_and_Wind_Share_json[i]
+        if dict['Country'] == feat['properties']["ADMIN"]:
+            value_1990 = dict["1990"]
+            value_2000 = dict["2000"]
+            value_2010 = dict["2010"]
+            value_2017 = dict["2017"]
+    if value_1990:
+        feat['properties']["1990"] = value_1990
+    if value_2000:
+        feat['properties']["2000"] = value_2000
+    if value_2010:
+        feat['properties']["2010"] = value_2010
+    if value_2017:
+        feat['properties']["2017"] = value_2017
+
+Solar_and_Wind_Share_api = countries_solar_wind_share
+
+# total elec production API
+with open('static/data/countries.geojson', 'r') as f:
+    countries_total_elec_prod = json.load(f)
+
+Total_Electricity_Prod_df = pd.read_sql("SELECT * FROM Total_Electricity_Production", engine)
+Total_Electricity_Prod_df_merged=Total_Electricity_Prod_df.merge(latlon_df,how='inner',on='Country')
+Total_Electricity_Prod_json = json.loads(Total_Electricity_Prod_df_merged.to_json(orient = "records"))
+# Loop over GeoJSON features and add the new properties
+for feat in countries_total_elec_prod['features']:
+    value_1990 = "NA"
+    value_2000 = "NA"
+    value_2010 = "NA"
+    value_2017 = "NA"
+    for i in range(len(Total_Electricity_Prod_json)):
+        dict = Total_Electricity_Prod_json[i]
+        if dict['Country'] == feat['properties']["ADMIN"]:
+            value_1990 = dict["1990"]
+            value_2000 = dict["2000"]
+            value_2010 = dict["2010"]
+            value_2017 = dict["2017"]
+    if value_1990:
+        feat['properties']["1990"] = value_1990
+    if value_2000:
+        feat['properties']["2000"] = value_2000
+    if value_2010:
+        feat['properties']["2010"] = value_2010
+    if value_2017:
+        feat['properties']["2017"] = value_2017
+
+Total_Electricity_Prod_api = countries_total_elec_prod
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 import pandas
-from JSONify_APIs_v2_backup import CO2_emissions_api, Crude_NGL_Prod_api, Natural_Gas_Prod_api, Renewables_Electricity_Share_api, Solar_and_Wind_Share_api, Total_Electricity_Prod_api
+from JSONify_APIs import CO2_emissions_api, Crude_NGL_Prod_api, Natural_Gas_Prod_api, Renewables_Electricity_Share_api, Solar_and_Wind_Share_api, Total_Electricity_Prod_api
 import json
 
 app = Flask(__name__)
@@ -12,29 +12,29 @@ def index():
     return render_template("index.html")
 
 #api route for data to go to client
-@app.route("/ee/CO2")
+@app.route("/ee/co2")
 def co2():
     return jsonify(CO2_emissions_api)
 
 @app.route("/ee/liquids")
 def ngl():
-    return jsonify(json.loads(Crude_NGL_Prod_api))
+    return jsonify(Crude_NGL_Prod_api)
 
 @app.route("/ee/gas")
 def gas():
-    return jsonify(json.loads(Natural_Gas_Prod_api))
+    return jsonify(Natural_Gas_Prod_api)
 
 @app.route("/ee/renewables")
 def renew():
-    return jsonify(json.loads(Renewables_Electricity_Share_api))
+    return jsonify(Renewables_Electricity_Share_api)
 
 @app.route("/ee/solar_wind")
 def solar():
-    return jsonify(json.loads(Solar_and_Wind_Share_api))
+    return jsonify(Solar_and_Wind_Share_api)
 
 @app.route("/ee/total")
 def total():
-    return jsonify(json.loads(Total_Electricity_Prod_api))
+    return jsonify(Total_Electricity_Prod_api)
 
 if __name__ == "__main__":
     app.run()
